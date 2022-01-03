6e0187ebb986783a0e2057d6cb0735256672eff0
taylor d <taylor.delph@eyecuelab.com>
2022-01-03 10:59:47 -0800

    add image and css object

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
+import img from './../img/img.jpeg'
 
 export default function Header() {
   return (
-    <h1>Halp! Q</h1>
+    <>
+      <h1>Halp! Q</h1>
+      <img src={img} alt="colors" style={styledImage} />
+    </>
   );
+}
+
+const styledImage = {
+  height: 'auto',
+  maxWidth: '50%'
 }
