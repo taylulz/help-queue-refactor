780bcc0b754716352e0b90c98de804b6ea7ffba6
taylor d <taylor.delph@eyecuelab.com>
2022-01-17 15:25:07 -0800

    finish testing combined root reducer

--- src/__tests__/index-reducer.test.js
+++ src/__tests__/index-reducer.test.js
@@ -22,4 +22,24 @@ describe("rootReducer", () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null }));
   });
 
+  test('Check ADD_TICKET action works for ticketListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_TICKET',
+      names: 'name1 and name2',
+      location: '4b',
+      issue: 'issue 1',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
+
 });
